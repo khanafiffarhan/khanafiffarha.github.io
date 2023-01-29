69ccff03222c2222e5a07d3b8d6b75f3c7e44cad
khanafiffarhan <khanafiffarhan@gmail.com>
2023-01-30 01:21:53 +0600

    about

---
+++ pages/components/Title.tsx
@@ -0,0 +1,10 @@
+const Title = ({ title }) => {
+  return (
+    <div className="bg-black titttle w-fit p-[4px_21px_6px_21px] rounded-[3px]">
+      <p className="text-white  title capitalize  text-[1.5rem] ">{title}</p>
+
+    </div>
+  );
+};
+
+export default Title;
